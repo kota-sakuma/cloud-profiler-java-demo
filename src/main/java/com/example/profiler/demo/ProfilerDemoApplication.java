7207d8f3c2f8d358318c6c66806cd813c5550989
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-07-29 14:36:50 +0900

    chore: add Spring CLI template

---
+++ src/main/java/com/example/profiler/demo/ProfilerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.profiler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/profiler/demo/ProfilerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profiler.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

691c36ba8aa6ce6ae1593e37e631a9a16ae6e5f3
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-07-29 17:48:16 +0900

    feat: enable scheduling of app

--- src/main/java/com/example/profiler/demo/ProfilerDemoApplication.java
+++ src/main/java/com/example/profiler/demo/ProfilerDemoApplication.java
@@ -2,12 +2,13 @@ package com.example.profiler.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ProfilerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfilerDemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProfilerDemoApplication.class, args);
+  }
 }
